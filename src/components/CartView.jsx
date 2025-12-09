2b57da75a4de43757ec26089cefd7f415182790a
matias <Matiascosta2018@hotmail.com>
2025-12-09 18:25:50 -0300

    .

--- src/components/CartView.jsx
+++ src/components/CartView.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { CartContext } from '../context/CartContext';
 import { Link } from 'react-router-dom';
-import '../css/CartView.css'; 
+import '../css/Cartview.css'; 
 import Swal from 'sweetalert2';
 
 const CartView = () => {
